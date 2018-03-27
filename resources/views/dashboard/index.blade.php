f5915a0fd2138344e959adec555e99540a77655b
w1lldone <wildan.arrahman@gmail.com>
2018-03-27 12:10:17 +0700

    Implement dashboard

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,129 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="/css/bootstrap.css">
+    <link rel="stylesheet" href="/css/style.css">
+    <link href="/css/fontawesome-all.css" rel="stylesheet">
+    <!-- FONT -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600" rel="stylesheet"> 
+
+    <style type="text/css">
+      body {
+        font-family: 'Open Sans', sans-serif;
+        line-height: 1.1;
+      }
+    </style>
+    <title>BPP UGM Dashboard</title>
+  </head>
+  <body style="background: #eee; color: rgb(72, 72, 72);">
+    <nav class="navbar navbar-dark bg-light" id="Header-nav">
+      <div class="d-flex flex-wrap align-content-center" id="left-side--navbar">
+        <div class="d-flex align-items-center" id="title-case-div">
+          <h4 class="text-white m-0 py-2">UGM Publications From Minute to Minute</h4>
+        </div>
+      </div>
+    </nav>
+    <section class="container-fluid mt-3">
+      <div class="row align-items-center">
+        <div class="col-md-6 my-2">
+          <div class="d-flex bg-light rounded h-100 has-shadow">
+            <div class="bg-primary p-4 text-white rounded-left d-flex align-items-center">
+              <i class="far fa-file-alt fa-5x align-middle"></i>
+            </div>
+            <div class="p-3">
+              <h3>2000</h3>
+              <span class="text-muted">Article indexed by Scopus</span>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6 my-2">
+          <div class="d-flex bg-light rounded h-100 has-shadow">
+            <div class="bg-primary p-4 text-white rounded-left d-flex align-items-center">
+              <i class="far fa-file-alt fa-5x align-middle"></i>
+            </div>
+            <div class="p-3">
+              <h3>1200</h3>
+              <span class="text-muted">Article indexed by WOS, Ebsco, Microsoft Academic, and Pubmed</span>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6 my-2">
+          <div class="d-flex bg-light rounded h-100 has-shadow">
+            <div class="bg-primary p-4 text-white rounded-left d-flex align-items-center">
+              <i class="far fa-file-alt fa-5x align-middle"></i>
+            </div>
+            <div class="p-3">
+              <h3>560</h3>
+              <span class="text-muted">Books published</span>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6 my-2">
+          <div class="d-flex bg-light rounded h-100 has-shadow">
+            <div class="bg-primary p-4 text-white rounded-left d-flex align-items-center">
+              <i class="far fa-file-alt fa-5x align-middle"></i>
+            </div>
+            <div class="p-3">
+              <h3>422</h3>
+              <span class="text-muted">Hak cipta terdaftar</span>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6 my-2">
+          <div class="d-flex bg-light rounded h-100 has-shadow">
+            <div class="bg-primary p-4 text-white rounded-left d-flex align-items-center">
+              <i class="far fa-file-alt fa-5x align-middle"></i>
+            </div>
+            <div class="p-3">
+              <h3>1523</h3>
+              <span class="text-muted">Citations count on Scopus</span>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6 my-2">
+          <div class="d-flex bg-light rounded h-100 has-shadow">
+            <div class="bg-primary p-4 text-white rounded-left d-flex align-items-center">
+              <i class="far fa-file-alt fa-5x align-middle"></i>
+            </div>
+            <div class="p-3">
+              <h3>1802</h3>
+              <span class="text-muted">Citations count on WOS, Ebsco, Microsoft Academic, and Pubmed</span>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6 my-2">
+          <div class="d-flex bg-light rounded h-100 has-shadow">
+            <div class="bg-primary p-4 text-white rounded-left d-flex align-items-center">
+              <i class="far fa-file-alt fa-5x align-middle"></i>
+            </div>
+            <div class="p-3">
+              <h3>80%</h3>
+              <span class="text-muted">Verified Sinta Accounts</span>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6 my-2">
+          <div class="d-flex bg-light rounded h-100 has-shadow">
+            <div class="bg-primary p-4 text-white rounded-left d-flex align-items-center">
+              <i class="far fa-file-alt fa-5x align-middle"></i>
+            </div>
+            <div class="p-3">
+              <h3>2000</h3>
+              <span class="text-muted">Article indexed by WOS, Ebsco, Microsoft Academic, and Pubmed</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="js/bootstrap.js" crossorigin="anonymous"></script>
+  </body>
+</html>
