ae5eb6e5e7fc4653fd471e6151f4d9b66eaa1e47
w1lldone <wildan.arrahman@gmail.com>
2018-03-28 16:06:02 +0700

    Implement Auth

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center py-5">
+        <div class="col-md-8">
+          <h1 class="text-muted text-center">Sorry, Registration service is currently not available</h1>
+        </div>
+    </div>
+</div>
+@endsection
