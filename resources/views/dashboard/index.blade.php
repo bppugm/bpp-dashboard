12078028c437fe8a56a11302da4bd6736ae1db2f
w1lldone <wildan.arrahman@gmail.com>
2018-03-29 16:43:32 +0700

    Implement new interface

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,129 +1,184 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="{{ url('/css/bootstrap.css') }}">
-    <link rel="stylesheet" href="{{ url('/css/style.css') }}">
-    <link href="{{ url('/css/fontawesome-all.css') }}" rel="stylesheet">
-    <!-- FONT -->
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600" rel="stylesheet"> 
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="Content-Language" content="en" />
+    <meta name="msapplication-TileColor" content="#2d89ef">
+    <meta name="theme-color" content="#4188c9">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"/>
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="mobile-web-app-capable" content="yes">
+    <meta name="HandheldFriendly" content="True">
+    <meta name="MobileOptimized" content="320">
+    <link rel="icon" href="./favicon.ico" type="image/x-icon"/>
+    <link rel="shortcut icon" type="image/x-icon" href="./favicon.ico" />
+    <!-- Generated: 2018-03-27 13:25:03 +0200 -->
+    <title>Homepage - tabler.github.io - a responsive, flat and full featured admin template</title>
+    <link rel="stylesheet" href="/css/fontawesome-all.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,300i,400,400i,500,500i,600,600i,700,700i&amp;subset=latin-ext">
+    <script src="/assets/js/require.min.js"></script>
+    <script>
+      requirejs.config({
+          baseUrl: '.'
+          });
+    </script>
+    <!-- Dashboard Core -->
+    <link href="/assets/css/dashboard.css" rel="stylesheet" />
+    <script src="/assets/js/dashboard.js"></script>
+    <!-- c3.js Charts Plugin -->
+    <link href="/assets/plugins/charts-c3/plugin.css" rel="stylesheet" />
+    <script src="/assets/plugins/charts-c3/plugin.js"></script>
+    <!-- Google Maps Plugin -->
+    <link href="/assets/plugins/maps-google/plugin.css" rel="stylesheet" />
+    <script src="/assets/plugins/maps-google/plugin.js"></script>
+    <!-- Input Mask Plugin -->
+    <script src="/assets/plugins/input-mask/plugin.js"></script>
 
     <style type="text/css">
-      body {
-        font-family: 'Open Sans', sans-serif;
-        line-height: 1.1;
+      .card {
+        opacity: 0.8;
       }
     </style>
-    <title>BPP UGM Achievement from Minutes to Minutes</title>
   </head>
-  <body style="background: #eee; color: rgb(72, 72, 72);">
-    <nav class="navbar navbar-dark bg-blue-ugm has-shadow">
-      <div class="d-flex flex-wrap align-content-center" id="left-side--navbar">
-        <div class="d-flex align-items-center" id="title-case-div">
-          <h4 class="text-white m-0 py-2">BPP UGM's Achievements Minute by Minute</h4>
+  <body class="">
+    <div class="page" style="background: url('/assets/images/stocks/background.png') center center fixed no-repeat; background-size: cover; padding: 2.5rem 1rem 0 1rem;">
+      <div class="page-main">
+        <div class="page-content mt-0">
+          <div class="container-fluid">
+            <div class="row pb-5 mb-5">
+              <div class="col-12 py-5 text-center">
+                <h1 class="page-title text-white display-4 font-weight-bold m-auto" style="font-size: 3rem; text-shadow: 1px 3px 2px grey;">
+                  BPP UGM's Achievements Minute by Minute
+                </h1>
+              </div>
+            </div>
+            <div class="row row-cards">
+              <div class="col-md-3">
+                <div class="card h-100" style="border-radius: 1rem">
+                  <div class="card-body">
+                    <div class="text-left d-flex justify-content-between">
+                      <small class="text-muted"><i class="fa fa-cloud-upload-alt fa-fw"></i> 24 March 2018</small>
+                      <img src="/assets/images/icons/journal.svg" width="70">
+                    </div>
+                    <div class="display-4 font-weight-bold mb-3 text-primary">62</div>
+                    <div class="h5">Journal articles indexed in Scopus</div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-3 ">
+                <div class="card h-100" style="border-radius: 1rem">
+                  <div class="card-body">
+                    <div class="text-left d-flex justify-content-between">
+                      <small class="text-muted"><i class="fa fa-cloud-upload-alt fa-fw"></i> 24 March 2018</small>
+                      <img src="/assets/images/icons/journal.svg" width="70">
+                    </div>
+                    <div class="display-4 font-weight-bold mb-3 text-primary">62</div>
+                    <div class="h5">Journal articles indexed in WoS, EBSCO, Microsoft Academic, and Pubmed</div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-3">
+                <div class="card h-100" style="border-radius: 1rem">
+                  <div class="card-body">
+                    <div class="text-left d-flex justify-content-between">
+                      <small class="text-muted"><i class="fa fa-cloud-upload-alt fa-fw"></i> 24 March 2018</small>
+                      <img src="/assets/images/icons/proceeding.svg" width="70">
+                    </div>
+                    <div class="display-4 font-weight-bold mb-3 text-teal">62</div>
+                    <div class="h5">Proceedings indexed in Scopus</div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-3">
+                <div class="card h-100" style="border-radius: 1rem">
+                  <div class="card-body">
+                    <div class="text-left d-flex justify-content-between">
+                      <small class="text-muted"><i class="fa fa-cloud-upload-alt fa-fw"></i> 24 March 2018</small>
+                      <img src="/assets/images/icons/proceeding.svg" width="70">
+                    </div>
+                    <div class="display-4 font-weight-bold mb-3 text-teal">62</div>
+                    <div class="h5">Proceedings indexed in WoS, EBSCO, Microsoft Academic, and Pubmed</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="row row-cards mt-0">
+              <div class="col-md-3">
+                <div class="card h-100" style="border-radius: 1rem">
+                  <div class="card-body">
+                    <div class="text-left d-flex justify-content-between">
+                      <small class="text-muted"><i class="fa fa-cloud-upload-alt fa-fw"></i> 24 March 2018</small>
+                      <img src="/assets/images/icons/citation.svg" width="70">
+                    </div>
+                    <div class="display-4 font-weight-bold mb-3 text-orange">62</div>
+                    <div class="h5">Citations in Scopus</div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-3">
+                <div class="card h-100" style="border-radius: 1rem">
+                  <div class="card-body">
+                    <div class="text-left d-flex justify-content-between">
+                      <small class="text-muted"><i class="fa fa-cloud-upload-alt fa-fw"></i> 24 March 2018</small>
+                      <img src="/assets/images/icons/book.svg" width="70">
+                    </div>
+                    <div class="display-4 font-weight-bold mb-3 text-red">62</div>
+                    <div class="h5">Books published</div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-3">
+                <div class="card h-100" style="border-radius: 1rem">
+                  <div class="card-body">
+                    <div class="text-left d-flex justify-content-between">
+                      <small class="text-muted"><i class="fa fa-cloud-upload-alt fa-fw"></i> 24 March 2018</small>
+                      <img src="/assets/images/icons/copyright.svg" width="70">
+                    </div>
+                    <div class="display-4 font-weight-bold mb-3 text-yellow">62</div>
+                    <div class="h5">Copyrights registered</div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-3">
+                <div class="card h-100" style="border-radius: 1rem">
+                  <div class="card-body">
+                    <div class="text-left d-flex justify-content-between">
+                      <small class="text-muted"><i class="fa fa-cloud-upload-alt fa-fw"></i> 24 March 2018</small>
+                      <img src="/assets/images/icons/account.svg" width="70">
+                    </div>
+                    <div class="display-4 font-weight-bold mb-3 text-success">62</div>
+                    <div class="h5">Verified Sinta accounts</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </nav>
-    <section class="container-fluid mt-3">
-      <div class="row align-items-center">
-        <div class="col-md-6 my-2">
-          <div class="d-flex bg-light rounded h-100 has-shadow">
-            <div class="bg-blue-ugm p-4 text-white rounded-left d-flex align-items-center">
-              <i class="far fa-file-alt fa-5x align-middle"></i>
+      <footer class="footer bg-transparent border-0 text-white">
+        <div class="container">
+          <div class="row align-items-center flex-row-reverse">
+            <div class="col-auto ml-auto">
+              <div class="row align-items-center">
+                <div class="col-auto">
+                  <ul class="list-inline list-inline-dots mb-0">
+                    <li class="list-inline-item"><a href="./docs/index.html">Documentation</a></li>
+                    <li class="list-inline-item"><a href="./faq.html">FAQ</a></li>
+                  </ul>
+                </div>
+                <div class="col-auto">
+                  <a href="https://github.com/tabler/tabler" class="btn btn-outline-primary btn-sm">Source code</a>
+                </div>
+              </div>
             </div>
-            <div class="p-3">
-              <h2 class="data-number">{{ $data->article_scopus }}</h2>
-              <span class="text-muted">Journal articles indexed in Scopus</span>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-6 my-2">
-          <div class="d-flex bg-light rounded h-100 has-shadow">
-            <div class="bg-blue-ugm p-4 text-white rounded-left d-flex align-items-center">
-              <i class="far fa-file-alt fa-5x align-middle"></i>
-            </div>
-            <div class="p-3">
-              <h2 class="data-number">{{ $data->proceeding_scopus }}</h2>
-              <span class="text-muted">Proceedings indexed in Scopus</span>
+            <div class="col-12 col-lg-auto mt-3 mt-lg-0 text-center">
+              Copyright © 2018 <a href="http://publikasi.ugm.ac.id">Badan Penerbit dan Publikasi UGM</a>. Theme by <a href="https://codecalm.net" target="_blank">codecalm.net</a>.
             </div>
           </div>
         </div>
-        <div class="col-md-6 my-2">
-          <div class="d-flex bg-light rounded h-100 has-shadow">
-            <div class="bg-blue-ugm p-4 text-white rounded-left d-flex align-items-center">
-              <i class="far fa-file-alt fa-5x align-middle"></i>
-            </div>
-            <div class="p-3">
-              <h2 class="data-number">{{ $data->article_other }}</h2>
-              <span class="text-muted">Journal articles indexed in WoS, EBSCO, Microsoft Academic, and Pubmed</span>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-6 my-2">
-          <div class="d-flex bg-light rounded h-100 has-shadow">
-            <div class="bg-blue-ugm p-4 text-white rounded-left d-flex align-items-center">
-              <i class="far fa-file-alt fa-5x align-middle"></i>
-            </div>
-            <div class="p-3">
-              <h2 class="data-number">{{ $data->proceeding_other }}</h2>
-              <span class="text-muted">Proceedings indexed in W0S, EBSCO, Microsoft Academic, and Pubmed</span>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-6 my-2">
-          <div class="d-flex bg-light rounded h-100 has-shadow">
-            <div class="bg-blue-ugm p-4 text-white rounded-left d-flex align-items-center">
-              <i class="far fa-file-alt fa-5x align-middle"></i>
-            </div>
-            <div class="p-3">
-              <h2 class="data-number">{{ $data->citation_scopus }}</h2>
-              <span class="text-muted">Citations on Scopus</span>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-6 my-2">
-          <div class="d-flex bg-light rounded h-100 has-shadow">
-            <div class="bg-blue-ugm p-4 text-white rounded-left d-flex align-items-center">
-              <i class="far fa-file-alt fa-5x align-middle"></i>
-            </div>
-            <div class="p-3">
-              <h2 class="data-number">{{ $data->book }}</h2>
-              <span class="text-muted">Books published</span>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-6 my-2">
-          <div class="d-flex bg-light rounded h-100 has-shadow">
-            <div class="bg-blue-ugm p-4 text-white rounded-left d-flex align-items-center">
-              <i class="far fa-file-alt fa-5x align-middle"></i>
-            </div>
-            <div class="p-3">
-              <h2 class="data-number">{{ $data->copyright }}</h2>
-              <span class="text-muted">Copyrights registered</span>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-6 my-2">
-          <div class="d-flex bg-light rounded h-100 has-shadow">
-            <div class="bg-blue-ugm p-4 text-white rounded-left d-flex align-items-center">
-              <i class="far fa-file-alt fa-5x align-middle"></i>
-            </div>
-            <div class="p-3">
-              <h2 class="data-number">{{ $data->sinta_account }}/{{ $data->total_staff }}</h2>
-              <span class="text-muted">Verified Sinta accounts</span>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    {{-- <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script> --}}
-    {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script> --}}
-    <script src="{{ url('/js/bootstrap.js') }}" crossorigin="anonymous"></script>
+      </footer>
+    </div>
   </body>
 </html>
