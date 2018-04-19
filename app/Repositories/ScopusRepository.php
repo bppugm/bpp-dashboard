664e78b0315d7efcf78ffcc3feef32ee1760bc20
w1lldone <wildan.arrahman@gmail.com>
2018-04-19 15:21:41 +0700

    Add getProceeding method to Scopus Repository;

--- app/Repositories/ScopusRepository.php
+++ app/Repositories/ScopusRepository.php
@@ -13,14 +13,29 @@ class ScopusRepository extends GuzzleService
         'base' => 'https://api.elsevier.com/',
     ];
 
-    public $headers = [
-        'X-ELS-APIKey' => 'ee76c131d9bf443e5afffa9be30dd723'
-    ];
+    public function setRepoHeader()
+    {
+        $this->headers = [
+            'X-ELS-APIKey' => env('SCOPUS_API_KEY')
+        ];
+    }
 
-    public function search()
+    public function getArticle()
     {
         $this->query = [
             'query' => 'pubdatetxt(2018)AF-ID(60069380)DOCTYPE(ar OR ip)',
+            'count' => 1,
+        ];
+
+        $this->getResponse('GET', 'content/search/scopus');
+
+        return $this->data['search-results']['opensearch:totalResults'];
+    }
+
+    public function getProceeding()
+    {
+        $this->query = [
+            'query' => 'pubdatetxt(2018)AF-ID(60069380)DOCTYPE(cp)',
             'count' => 1,
         ];
 
