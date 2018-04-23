d8ff6c40072224831fb6c8b6138e080b0aae2f16
w1lldone <wildan.arrahman@gmail.com>
2018-04-23 16:02:22 +0700

    Add values to achevements table seeder and implement it

--- database/seeds/AchievementsTableSeeder.php
+++ database/seeds/AchievementsTableSeeder.php
@@ -12,15 +12,15 @@ class AchievementsTableSeeder extends Seeder
      */
     public function run()
     {
-        Achievement::create(['category_id' => 1, 'name' => 'article_scopus']);
-        Achievement::create(['category_id' => 1, 'name' => 'article_other']);
-        Achievement::create(['category_id' => 2, 'name' => 'proceeding_scopus']);
-        Achievement::create(['category_id' => 2, 'name' => 'proceeding_other']);
-        Achievement::create(['category_id' => 3, 'name' => 'book']);
-        Achievement::create(['category_id' => 4, 'name' => 'citation_scopus']);
-        Achievement::create(['category_id' => 4, 'name' => 'total_citation']);
-        Achievement::create(['category_id' => 4, 'name' => 'copyright']);
-        Achievement::create(['category_id' => 4, 'name' => 'sinta_account']);
-        Achievement::create(['category_id' => 4, 'name' => 'total_staff']);
+        Achievement::create(['category_id' => 1, 'name' => 'article_scopus', 'value' => 195]);
+        Achievement::create(['category_id' => 1, 'name' => 'article_other', 'value' => 364]);
+        Achievement::create(['category_id' => 2, 'name' => 'proceeding_scopus', 'value' => 101]);
+        Achievement::create(['category_id' => 2, 'name' => 'proceeding_other', 'value' => 2]);
+        Achievement::create(['category_id' => 3, 'name' => 'book', 'value' => 73]);
+        Achievement::create(['category_id' => 4, 'name' => 'citation_scopus', 'value' => 1838]);
+        Achievement::create(['category_id' => 4, 'name' => 'total_citation', 'value' => 62093]);
+        Achievement::create(['category_id' => 4, 'name' => 'copyright', 'value' => 65]);
+        Achievement::create(['category_id' => 4, 'name' => 'sinta_account', 'value' => 2081]);
+        Achievement::create(['category_id' => 4, 'name' => 'total_staff', 'value' => 2793]);
     }
 }
