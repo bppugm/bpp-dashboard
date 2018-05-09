840fa3089d659a0515333e1ad07e592bb4716b08
w1lldone <wildan.arrahman@gmail.com>
2018-05-09 14:03:43 +0700

    Fix resource URL

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -20,19 +20,19 @@
 
     <!-- Generated: 2018-03-27 13:25:03 +0200 -->
     <title>{{ env('APP_NAME') }}</title>
-    <link rel="stylesheet" href="{{ url('/css/fontawesome-all.css') }}">
+    <link rel="stylesheet" href="{{ url('css/fontawesome-all.css') }}">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700">
 
     <!-- Dashboard Core -->
-    <link href="{{ url('/css/dashboard.css') }}" rel="stylesheet" />
+    <link href="{{ url('css/dashboard.css') }}" rel="stylesheet" />
     {{-- JQUERY --}}
-    <script src="{{ url('/js/jquery-3.2.1.min.js') }}"></script>
+    <script src="{{ url('js/jquery-3.2.1.min.js') }}"></script>
 
     <!-- Odometr includes -->
     {{-- <link rel="stylesheet" href="http://github.hubspot.com/odometer/themes/odometer-theme-car.css" /> --}}
     {{-- <script src="http://github.hubspot.com/odometer/odometer.js"></script> --}}
-    <script src="{{ url('/js/odometer.min.js') }}"></script>
-    <link rel="stylesheet" href="{{ url('/css/odometer-theme-default.css') }}"/>
+    <script src="{{ url('js/odometer.min.js') }}"></script>
+    <link rel="stylesheet" href="{{ url('css/odometer-theme-default.css') }}"/>
 
     {{-- PUSHER LIBRARY --}}
     <script src="https://js.pusher.com/4.1/pusher.min.js"></script>
@@ -52,7 +52,7 @@
           <div class="container-fluid">
             <div class="row pb-5 mb-5">
               <div class="col-12 py-2 d-flex align-items-center justify-content-center">
-                <img src="{{ url('/images/logos/signature-logo-ugm.svg') }}" width="75" class="mr-3">
+                <img src="{{ url('images/logos/signature-logo-ugm.svg') }}" width="75" class="mr-3">
                 <h1 class="page-title text-primary display-4 font-weight-bold" style="font-size: 3rem;">
                   UGM's Achievements Minute by Minute
                 </h1>
@@ -64,7 +64,7 @@
                   <div class="card-body">
                     <div class="text-left d-flex justify-content-between">
                       <small id="article_scopus_date" class="text-muted"><i class="fa fa-cloud-upload-alt fa-fw"></i> {{ $data['article_scopus']->updated_at->format('j F Y') }}</small>
-                      <img src="{{ url('/images/icons/journal.svg') }}" width="70">
+                      <img src="{{ url('images/icons/journal.svg') }}" width="70">
                     </div>
                     <div class="font-weight-bold mb-3 odometer text-primary" id="article_scopus">{{ $data['article_scopus']->value }}</div>
                     <div class="h5">Journal articles indexed in Scopus</div>
@@ -76,7 +76,7 @@
                   <div class="card-body">
                     <div class="text-left d-flex justify-content-between">
                       <small id="article_other_date" class="text-muted"><i class="fa fa-cloud-upload-alt fa-fw"></i> {{ $data['article_other']->updated_at->format('j F Y') }}</small>
-                      <img src="{{ url('/images/icons/journal.svg') }}" width="70">
+                      <img src="{{ url('images/icons/journal.svg') }}" width="70">
                     </div>
                     <div class="font-weight-bold mb-3 odometer text-primary" id="article_other">{{ $data['article_other']->value }}</div>
                     <div class="h5">Journal articles indexed in WoS, EBSCO, Proquest, Microsoft Academic, and Pubmed</div>
@@ -88,7 +88,7 @@
                   <div class="card-body">
                     <div class="text-left d-flex justify-content-between">
                       <small id="proceeding_scopus_date" class="text-muted"><i class="fa fa-cloud-upload-alt fa-fw"></i> {{ $data['proceeding_scopus']->updated_at->format('j F Y') }}</small>
-                      <img src="{{ url('/images/icons/proceeding.svg') }}" width="70">
+                      <img src="{{ url('images/icons/proceeding.svg') }}" width="70">
                     </div>
                     <div class="font-weight-bold mb-3 odometer text-teal" id="proceeding_scopus">{{ $data['proceeding_scopus']->value }}</div>
                     <div class="h5">Proceedings indexed in Scopus</div>
@@ -100,7 +100,7 @@
                   <div class="card-body">
                     <div class="text-left d-flex justify-content-between">
                       <small id="proceeding_other_date" class="text-muted"><i class="fa fa-cloud-upload-alt fa-fw"></i> {{ $data['proceeding_other']->updated_at->format('j F Y') }}</small>
-                      <img src="{{ url('/images/icons/proceeding.svg') }}" width="70">
+                      <img src="{{ url('images/icons/proceeding.svg') }}" width="70">
                     </div>
                     <div class="font-weight-bold mb-3 odometer text-teal" id="proceeding_other">{{ $data['proceeding_other']->value }}</div>
                     <div class="h5">Proceedings indexed in WoS, EBSCO, Proquest, Microsoft Academic, and Pubmed</div>
@@ -114,7 +114,7 @@
                   <div class="card-body">
                     <div class="text-left d-flex justify-content-between">
                       <small id="citation_scopus_date" class="text-muted"><i class="fa fa-cloud-upload-alt fa-fw"></i> {{ $data['citation_scopus']->updated_at->format('j F Y') }}</small>
-                      <img src="{{ url('/images/icons/citation.svg') }}" width="70">
+                      <img src="{{ url('images/icons/citation.svg') }}" width="70">
                     </div>
                     <div class="font-weight-bold mb-3 text-orange">
                       <span class="odometer" id="citation_scopus">{{ $data['citation_scopus']->value }}</span>
@@ -130,7 +130,7 @@
                   <div class="card-body">
                     <div class="text-left d-flex justify-content-between">
                       <small id="book_date" class="text-muted"><i class="fa fa-cloud-upload-alt fa-fw"></i> {{ $data['book']->updated_at->format('j F Y') }}</small>
-                      <img src="{{ url('/images/icons/book.svg') }}" width="70">
+                      <img src="{{ url('images/icons/book.svg') }}" width="70">
                     </div>
                     <div class="font-weight-bold mb-3 odometer text-red" id="book">{{ $data['book']->value }}</div>
                     <div class="h5">Books published</div>
@@ -142,7 +142,7 @@
                   <div class="card-body">
                     <div class="text-left d-flex justify-content-between">
                       <small id="copyright_date" class="text-muted"><i class="fa fa-cloud-upload-alt fa-fw"></i> {{ $data['copyright']->updated_at->format('j F Y') }}</small>
-                      <img src="{{ url('/images/icons/copyright.svg') }}" width="70">
+                      <img src="{{ url('images/icons/copyright.svg') }}" width="70">
                     </div>
                     <div class="font-weight-bold mb-3 odometer text-indigo" id="copyright">{{ $data['copyright']->value }}</div>
                     <div class="h5">Copyrights registered</div>
@@ -154,7 +154,7 @@
                   <div class="card-body">
                     <div class="text-left d-flex justify-content-between">
                       <small id="sinta_account_date" class="text-muted"><i class="fa fa-cloud-upload-alt fa-fw"></i> {{ $data['sinta_account']->updated_at->format('j F Y') }}</small>
-                      <img src="{{ url('/images/icons/account.svg') }}" width="70">
+                      <img src="{{ url('images/icons/account.svg') }}" width="70">
                     </div>
                     <div class="font-weight-bold mb-3 text-success">
                       <span class="odometer" id="sinta_account">{{ $data['sinta_account']->value }}</span>
