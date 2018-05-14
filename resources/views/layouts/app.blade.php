09698a9a32357e38c47221a298071d85039620ec
w1lldone <wildan.arrahman@gmail.com>
2018-05-14 22:03:25 +0700

    Fix fontawesome url in app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" href="/font-awesome/css/fontawesome-all.css">
+    <link rel="stylesheet" href="{{ asset('font-awesome/css/fontawesome-all.css') }}">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
