46bcddd62d90269326a96e983a4ced652066b1ee
w1lldone <wildan.arrahman@gmail.com>
2018-05-14 13:14:07 +0700

    Refactor views

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,185 +1,113 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta http-equiv="Content-Language" content="en" />
-    
-    {{-- FAVICONS --}}
-    <link rel="apple-touch-icon" sizes="180x180" href="{{url('favicons/apple-touch-icon.png')}}">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{url('favicons/favicon-32x32.png')}}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{url('favicons/favicon-16x16.png')}}">
-    <link rel="manifest" href="{{url('favicons/site.webmanifest')}}">
-    <link rel="mask-icon" href="{{url('favicons/safari-pinned-tab.svg')}}" color="#5bbad5">
-    <link rel="shortcut icon" href="{{url('favicons/favicon.ico')}}">
-    <meta name="msapplication-TileColor" content="#da532c">
-    <meta name="msapplication-config" content="{{url('favicons/browserconfig.xml')}}">
-    <meta name="theme-color" content="#ffffff">
+@extends('layouts.app')
 
-
-    <!-- Generated: 2018-03-27 13:25:03 +0200 -->
-    <title>{{ env('APP_NAME') }}</title>
-    <link rel="stylesheet" href="font-awesome/css/fontawesome-all.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700">
-
-    <!-- Dashboard Core -->
-    <link href="css/dashboard.css" rel="stylesheet" />
-    {{-- JQUERY --}}
-    <script src="js/jquery-3.2.1.min.js"></script>
-
-    <!-- Odometr includes -->
-    {{-- <link rel="stylesheet" href="http://github.hubspot.com/odometer/themes/odometer-theme-car.css" /> --}}
-    {{-- <script src="http://github.hubspot.com/odometer/odometer.js"></script> --}}
-    <script src="js/odometer.min.js"></script>
-    <link rel="stylesheet" href="css/odometer-theme-default.css"/>
-
-    {{-- PUSHER LIBRARY --}}
-    <script src="https://js.pusher.com/4.1/pusher.min.js"></script>
-    @include('script.pusher')
-
-    <style type="text/css">
-      .odometer {
-        font-size: 2.2rem;
-      }
-    </style>
-
-  </head>
-  <body class="pt-5">
-    <div class="page">
-      <div class="page-main">
-        <div class="page-content mt-0">
-          <div class="container-fluid">
-            <div class="row pb-5 mb-5">
-              <div class="col-12 py-2 d-flex align-items-center justify-content-center">
-                <img src="{{ url('images/logos/signature-logo-ugm.svg') }}" width="75" class="mr-3">
-                <h1 class="page-title text-primary display-4 font-weight-bold" style="font-size: 3rem;">
-                  UGM's Achievements Minute by Minute
-                </h1>
-              </div>
+@section('content')
+  <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-md-8">
+          @if (session('status'))
+            <div class="alert alert-success">
+                {{ session('status') }}
             </div>
-            <div class="row row-cards">
-              <div class="col-md-3">
-                <div class="card h-100" style="border-radius: 1rem">
-                  <div class="card-body">
-                    <div class="text-left d-flex justify-content-between">
-                      <small id="article_scopus_date" class="text-muted"><i class="fa fa-cloud-upload-alt fa-fw"></i> {{ $data['article_scopus']->updated_at->format('j F Y') }}</small>
-                      <img src="{{ url('images/icons/journal.svg') }}" width="70">
-                    </div>
-                    <div class="font-weight-bold mb-3 odometer text-primary" id="article_scopus">{{ $data['article_scopus']->value }}</div>
-                    <div class="h5">Journal articles indexed in Scopus</div>
-                  </div>
-                </div>
-              </div>
-              <div class="col-md-3 ">
-                <div class="card h-100" style="border-radius: 1rem">
-                  <div class="card-body">
-                    <div class="text-left d-flex justify-content-between">
-                      <small id="article_other_date" class="text-muted"><i class="fa fa-cloud-upload-alt fa-fw"></i> {{ $data['article_other']->updated_at->format('j F Y') }}</small>
-                      <img src="{{ url('images/icons/journal.svg') }}" width="70">
-                    </div>
-                    <div class="font-weight-bold mb-3 odometer text-primary" id="article_other">{{ $data['article_other']->value }}</div>
-                    <div class="h5">Journal articles indexed in WoS, EBSCO, Proquest, Microsoft Academic, and Pubmed</div>
-                  </div>
-                </div>
-              </div>
-              <div class="col-md-3">
-                <div class="card h-100" style="border-radius: 1rem">
-                  <div class="card-body">
-                    <div class="text-left d-flex justify-content-between">
-                      <small id="proceeding_scopus_date" class="text-muted"><i class="fa fa-cloud-upload-alt fa-fw"></i> {{ $data['proceeding_scopus']->updated_at->format('j F Y') }}</small>
-                      <img src="{{ url('images/icons/proceeding.svg') }}" width="70">
-                    </div>
-                    <div class="font-weight-bold mb-3 odometer text-teal" id="proceeding_scopus">{{ $data['proceeding_scopus']->value }}</div>
-                    <div class="h5">Proceedings indexed in Scopus</div>
-                  </div>
-                </div>
-              </div>
-              <div class="col-md-3">
-                <div class="card h-100" style="border-radius: 1rem">
-                  <div class="card-body">
-                    <div class="text-left d-flex justify-content-between">
-                      <small id="proceeding_other_date" class="text-muted"><i class="fa fa-cloud-upload-alt fa-fw"></i> {{ $data['proceeding_other']->updated_at->format('j F Y') }}</small>
-                      <img src="{{ url('images/icons/proceeding.svg') }}" width="70">
-                    </div>
-                    <div class="font-weight-bold mb-3 odometer text-teal" id="proceeding_other">{{ $data['proceeding_other']->value }}</div>
-                    <div class="h5">Proceedings indexed in WoS, EBSCO, Proquest, Microsoft Academic, and Pubmed</div>
-                  </div>
-                </div>
-              </div>
+          @endif
+          @if ($errors->first('data.*'))
+            <div class="alert alert-warning">
+              Whoops, it looks like you have entered incorrect value. Please try again.
             </div>
-            <div class="row row-cards mt-0">
-              <div class="col-md-3">
-                <div class="card h-100" style="border-radius: 1rem">
-                  <div class="card-body">
-                    <div class="text-left d-flex justify-content-between">
-                      <small id="citation_scopus_date" class="text-muted"><i class="fa fa-cloud-upload-alt fa-fw"></i> {{ $data['citation_scopus']->updated_at->format('j F Y') }}</small>
-                      <img src="{{ url('images/icons/citation.svg') }}" width="70">
-                    </div>
-                    <div class="font-weight-bold mb-3 text-orange">
-                      <span class="odometer" id="citation_scopus">{{ $data['citation_scopus']->value }}</span>
-                      /
-                      <span class="odometer" style="font-size: 1.5rem;" id="total_citation">{{ $data['total_citation']->value }}</span>
-                    </div>
-                    <div class="h5">Citations on Scopus in {{ date('Y') }} and accumulation</div>
-                  </div>
-                </div>
-              </div>
-              <div class="col-md-3">
-                <div class="card h-100" style="border-radius: 1rem">
-                  <div class="card-body">
-                    <div class="text-left d-flex justify-content-between">
-                      <small id="book_date" class="text-muted"><i class="fa fa-cloud-upload-alt fa-fw"></i> {{ $data['book']->updated_at->format('j F Y') }}</small>
-                      <img src="{{ url('images/icons/book.svg') }}" width="70">
-                    </div>
-                    <div class="font-weight-bold mb-3 odometer text-red" id="book">{{ $data['book']->value }}</div>
-                    <div class="h5">Books published</div>
-                  </div>
-                </div>
-              </div>
-              <div class="col-md-3">
-                <div class="card h-100" style="border-radius: 1rem">
-                  <div class="card-body">
-                    <div class="text-left d-flex justify-content-between">
-                      <small id="copyright_date" class="text-muted"><i class="fa fa-cloud-upload-alt fa-fw"></i> {{ $data['copyright']->updated_at->format('j F Y') }}</small>
-                      <img src="{{ url('images/icons/copyright.svg') }}" width="70">
-                    </div>
-                    <div class="font-weight-bold mb-3 odometer text-indigo" id="copyright">{{ $data['copyright']->value }}</div>
-                    <div class="h5">Copyrights registered</div>
-                  </div>
-                </div>
-              </div>
-              <div class="col-md-3">
-                <div class="card h-100" style="border-radius: 1rem">
-                  <div class="card-body">
-                    <div class="text-left d-flex justify-content-between">
-                      <small id="sinta_account_date" class="text-muted"><i class="fa fa-cloud-upload-alt fa-fw"></i> {{ $data['sinta_account']->updated_at->format('j F Y') }}</small>
-                      <img src="{{ url('images/icons/account.svg') }}" width="70">
-                    </div>
-                    <div class="font-weight-bold mb-3 text-success">
-                      <span class="odometer" id="sinta_account">{{ $data['sinta_account']->value }}</span>
-                      <span>/</span>
-                      <span class="odometer" style="font-size: 1.5rem !important;" id="total_staff">{{ $data['total_staff']->value }}</span>
-                    </div>
-                    <div class="h5">Verified Sinta accounts and total UGM staff</div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+          @endif
         </div>
       </div>
-      <footer class="footer" style="font-size: 1rem">
-        <div class="container-fluid">
-          <div class="row align-items-center">
-            <div class="col-12 mt-3 mt-lg-0 text-center">
-              <a href="#">About</a> | <a href="#">Site Credits</a> | <a href="#">BPP UGM</a> <br>
-              Copyright © 2018 <a href="http://publikasi.ugm.ac.id" target="_blank">Badan Penerbit dan Publikasi UGM</a>. 
-              {{-- Theme by <a href="https://codecalm.net" target="_blank">codecalm.net</a>. --}}
+      <div class="row justify-content-center py-2">
+        <div class="col-md-8">
+          <div class="card">
+            <div class="card-header">Edit data</div>
+            <div class="card-body">
+              <form method="POST" action="{{ route('home.update') }}">
+                @csrf
+                @method('PUT')
+                <div class="form-group">
+                  <label for="staticEmail" class="form-label">Journal articles indexed in WoS, EBSCO, Microsoft Academic, and Pubmed</label>
+                  <input type="number" class="form-control @if($errors->has('data.article_other')) is-invalid @endif" name="data[article_other]" value="{{ $data['article_other']->value }}">
+                  <div class="d-flex justify-content-between mt-1">
+                    <small class="text-danger">{{ $errors->first('data.article_other') }}</small>
+                    <small class="text-muted"><i class="far fa-clock fa-fw"></i> {{ $data['article_other']->updated_at->format('j F Y h:i') }}</small>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label for="staticEmail" class="form-label">Proceedings indexed in WoS, EBSCO, Microsoft Academic, and Pubmed</label>
+                  <input type="number" class="form-control @if($errors->has('data.proceeding_other')) is-invalid @endif" name="data[proceeding_other]" value="{{ $data['proceeding_other']->value }}">
+                  <div class="d-flex justify-content-between mt-1">
+                    <small class="text-danger">{{ $errors->first('data.proceeding_other') }}</small>
+                    <small class="text-muted"><i class="far fa-clock fa-fw"></i> {{ $data['proceeding_other']->updated_at->format('j F Y h:i') }}</small>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label for="staticEmail" class="form-label">Citation in Scopus in {{ date('Y') }}</label>
+                  <input type="number" class="form-control @if($errors->has('data.citation_scopus')) is-invalid @endif" name="data[citation_scopus]" value="{{ $data['citation_scopus']->value }}">
+                  <div class="d-flex justify-content-between mt-1">
+                    <small class="text-danger">{{ $errors->first('data.citation_scopus') }}</small>
+                    <small class="text-muted"><i class="far fa-clock fa-fw"></i> {{ $data['citation_scopus']->updated_at->format('j F Y h:i') }}</small>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label for="staticEmail" class="form-label">Total citation in Scopus</label>
+                  <input type="number" class="form-control @if($errors->has('data.total_citation')) is-invalid @endif" name="data[total_citation]" value="{{ $data['total_citation']->value }}">
+                  <div class="d-flex justify-content-between mt-1">
+                    <small class="text-danger">{{ $errors->first('data.total_citation') }}</small>
+                    <small class="text-muted"><i class="far fa-clock fa-fw"></i> {{ $data['total_citation']->updated_at->format('j F Y h:i') }}</small>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label for="staticEmail" class="form-label">Books published</label>
+                  <input type="number" class="form-control @if($errors->has('data.book')) is-invalid @endif" name="data[book]" value="{{ $data['book']->value }}">
+                  <div class="d-flex justify-content-between mt-1">
+                    <small class="text-danger">{{ $errors->first('data.book') }}</small>
+                    <small class="text-muted"><i class="far fa-clock fa-fw"></i> {{ $data['book']->updated_at->format('j F Y h:i') }}</small>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label for="staticEmail" class="form-label">Copyrights registered</label>
+                  <input type="number" class="form-control @if($errors->has('data.copyright')) is-invalid @endif" name="data[copyright]" value="{{ $data['copyright']->value }}">
+                  <div class="d-flex justify-content-between mt-1">
+                    <small class="text-danger">{{ $errors->first('data.copyright') }}</small>
+                    <small class="text-muted"><i class="far fa-clock fa-fw"></i> {{ $data['copyright']->updated_at->format('j F Y h:i') }}</small>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label for="staticEmail" class="form-label">Verified Sinta accounts</label>
+                  <input type="number" class="form-control @if($errors->has('data.sinta_account')) is-invalid @endif" name="data[sinta_account]" value="{{ $data['sinta_account']->value }}">
+                  <div class="d-flex justify-content-between mt-1">
+                    <small class="text-danger">{{ $errors->first('data.sinta_account') }}</small>
+                    <small class="text-muted"><i class="far fa-clock fa-fw"></i> {{ $data['sinta_account']->updated_at->format('j F Y h:i') }}</small>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label for="staticEmail" class="form-label">Total Sinta accounts</label>
+                  <input type="number" class="form-control @if($errors->has('data.total_staff')) is-invalid @endif" name="data[total_staff]" value="{{ $data['total_staff']->value }}">
+                  <div class="d-flex justify-content-between mt-1">
+                    <small class="text-danger">{{ $errors->first('data.total_staff') }}</small>
+                    <small class="text-muted"><i class="far fa-clock fa-fw"></i> {{ $data['total_staff']->updated_at->format('j F Y h:i') }}</small>
+                  </div>
+                </div>
+                <div class="form-group border-bottom mt-4">
+                  <b>These fields below have implemented Scheduled Update. There's no need to update it manually.</b>
+                </div>
+                <div class="form-group">
+                  <label for="staticEmail" class="form-label">Journal articles indexed in Scopus</label>
+                  <input type="number" class="form-control" disabled value="{{ $data['article_scopus']->value }}">
+                  <small class="text-muted float-right"><i class="far fa-clock fa-fw"></i> {{ $data['article_scopus']->updated_at->format('j F Y h:i') }}</small>
+                </div>
+                <div class="form-group">
+                  <label for="staticEmail" class="form-label">Proceedings indexed in Scopus</label>
+                  <input type="number" class="form-control" disabled value="{{ $data['proceeding_scopus']->value }}">
+                  <small class="text-muted float-right"><i class="far fa-clock fa-fw"></i> {{ $data['proceeding_scopus']->updated_at->format('j F Y h:i') }}</small>
+                </div>
+                <div class="form-group">
+                  <button class="btn btn-success" type="submit">Save changes</button>
+                </div>
+              </form>
             </div>
           </div>
-        </div>
-      </footer>
+      </div>
     </div>
-  </body>
-</html>
+  </div>
+@endsection
