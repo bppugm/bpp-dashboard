b11449dd27592b51676d4a35243119efd89f8563
w1lldone <wildan.arrahman@gmail.com>
2018-05-14 16:35:47 +0700

    Add curl option

--- app/Services/GuzzleService.php
+++ app/Services/GuzzleService.php
@@ -154,7 +154,7 @@ class GuzzleService
         }
 
         $body['curl'] = [
-            // CURLOPT_CAINFO => env('CURL_SSLCERT'),
+            CURLOPT_CAINFO => env('CURL_SSLCERT'),
             // CURLOPT_SSL_VERIFYPEER => false,
             // CURLOPT_SSL_VERIFYHOST => false,
         ];
