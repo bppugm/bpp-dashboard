88f1e10ba68aeb3ee79afd75b7929b34be2faf50
w1lldone <wildan.arrahman@gmail.com>
2018-05-16 14:09:16 +0700

    Add active state on sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -2,8 +2,8 @@
   <div class="card-body">
     <h5 class="card-title">Navigation</h5>
     <nav class="nav flex-column bg-white nav-pills">
-      <a class="nav-link" href="{{ route('home') }}"><i class="fa fa-edit fa-fw"></i> Edit manually</a>
-      <a class="nav-link" href="{{ route('scheduled.index') }}"><i class="fa fa-history fa-fw"></i> Scheduled update</a>
+      <a class="nav-link @if(url()->current() == route('home')) active  @endif" href="{{ route('home') }}"><i class="fa fa-edit fa-fw"></i> Edit manually</a>
+      <a class="nav-link @if(url()->current() == route('scheduled.index')) active  @endif" href="{{ route('scheduled.index') }}"><i class="fa fa-history fa-fw"></i> Scheduled update</a>
     </nav>
   </div>
 </div>
