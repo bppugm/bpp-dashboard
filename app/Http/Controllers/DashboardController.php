2b2c59061d536f866ef15a3999663a6a5530b168
w1lldone <wildan.arrahman@gmail.com>
2019-02-15 16:07:33 +0700

    Get all data in Index Action

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -9,7 +9,7 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        $data = Achievement::getData();
+        $data = Achievement::all();
 
         return view('home', compact('data'));
     }
