65dcfc9c386100863980160e9207fb143e30c8cd
w1lldone <wildan.arrahman@gmail.com>
2019-02-21 09:27:14 +0700

    Import pusher keys from views

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -50,7 +50,7 @@ window.Pusher = require('pusher-js');
 
 window.Echo = new Echo({
     broadcaster: 'pusher',
-    key: process.env.MIX_PUSHER_APP_KEY,
-    cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+    key: window.config.pusherAppKey,
+    cluster: window.config.pusherAppCluster,
     encrypted: true
 });
