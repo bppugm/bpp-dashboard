b98ec3de2e5e940500213fd4bce245ca51aaada7
w1lldone <wildan.arrahman@gmail.com>
2019-02-21 09:36:51 +0700

    Fix Achievement seeder value

--- database/seeds/AchievementsTableSeeder.php
+++ database/seeds/AchievementsTableSeeder.php
@@ -19,7 +19,7 @@ class AchievementsTableSeeder extends Seeder
         Achievement::create(['category_id' => 2, 'name' => 'publication_ma', 'value' => 2146]);
         Achievement::create(['category_id' => 2, 'name' => 'publication_other', 'value' => 814]);
         Achievement::create(['category_id' => 3, 'name' => 'book', 'value' => 153]);
-        Achievement::create(['category_id' => 4, 'name' => 'citation_scopus', 'value' => 4352]);
+        Achievement::create(['category_id' => 4, 'name' => 'citation_scopus', 'value' => 7805]);
         Achievement::create(['category_id' => 4, 'name' => 'total_citation', 'value' => 81153, 'automated' => true]);
         Achievement::create(['category_id' => 4, 'name' => 'journal_scopus', 'value' => 5]);
         Achievement::create(['category_id' => 4, 'name' => 'journal_wos', 'value' => 4]);
