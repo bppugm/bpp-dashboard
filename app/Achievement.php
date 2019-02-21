2c7a8049dd718b23efa3aa7c9db8b00638509a33
w1lldone <wildan.arrahman@gmail.com>
2019-02-21 08:10:56 +0700

    Remove getData method

--- app/Achievement.php
+++ app/Achievement.php
@@ -8,24 +8,9 @@ class Achievement extends Model
 {
     protected $guarded = ['id'];
     protected $appends = ['formatted_updated_at'];
-
-    public static function getData($data = [])
-    {
-        $achievement = new Achievement;
-
-        $data['article_scopus'] = $achievement->where('name', 'article_scopus')->first();
-        $data['article_other'] = $achievement->where('name', 'article_other')->first();
-        $data['proceeding_scopus'] = $achievement->where('name', 'proceeding_scopus')->first();
-        $data['proceeding_other'] = $achievement->where('name', 'proceeding_other')->first();
-        $data['book'] = $achievement->where('name', 'book')->first();
-        $data['citation_scopus'] = $achievement->where('name', 'citation_scopus')->first();
-        $data['total_citation'] = $achievement->where('name', 'total_citation')->first();
-        $data['copyright'] = $achievement->where('name', 'copyright')->first();
-        $data['sinta_account'] = $achievement->where('name', 'sinta_account')->first();
-        $data['total_staff'] = $achievement->where('name', 'total_staff')->first();
-
-        return $data;
-    }
+    protected $casts = [
+        'value' => 'integer',
+    ];
 
     public function getFormattedUpdatedAtAttribute(){
         return $this->updated_at->format('j F Y');
