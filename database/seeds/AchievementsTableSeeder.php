17f7fb9698a4bb7a56b0ccdb8ae3f2389e3ac96c
w1lldone <wildan.arrahman@gmail.com>
2019-02-21 09:25:36 +0700

    Fix achievement seeder value

--- database/seeds/AchievementsTableSeeder.php
+++ database/seeds/AchievementsTableSeeder.php
@@ -15,14 +15,15 @@ class AchievementsTableSeeder extends Seeder
         Achievement::truncate();
         
         Achievement::create(['category_id' => 1, 'name' => 'publication_scopus', 'value' => 195, 'automated' => true]);
-        Achievement::create(['category_id' => 1, 'name' => 'publication_wos', 'value' => 364]);
-        Achievement::create(['category_id' => 2, 'name' => 'publication_ma', 'value' => 101]);
-        Achievement::create(['category_id' => 2, 'name' => 'publication_other', 'value' => 2]);
-        Achievement::create(['category_id' => 3, 'name' => 'book', 'value' => 73]);
-        Achievement::create(['category_id' => 4, 'name' => 'citation_scopus', 'value' => 1838]);
-        Achievement::create(['category_id' => 4, 'name' => 'total_citation', 'value' => 62093, 'automated' => true]);
-        Achievement::create(['category_id' => 4, 'name' => 'journal_scopus', 'value' => 65]);
-        Achievement::create(['category_id' => 4, 'name' => 'sinta_account', 'value' => 2081, 'automated' => true]);
+        Achievement::create(['category_id' => 1, 'name' => 'publication_wos', 'value' => 1146]);
+        Achievement::create(['category_id' => 2, 'name' => 'publication_ma', 'value' => 2146]);
+        Achievement::create(['category_id' => 2, 'name' => 'publication_other', 'value' => 814]);
+        Achievement::create(['category_id' => 3, 'name' => 'book', 'value' => 153]);
+        Achievement::create(['category_id' => 4, 'name' => 'citation_scopus', 'value' => 4352]);
+        Achievement::create(['category_id' => 4, 'name' => 'total_citation', 'value' => 81153, 'automated' => true]);
+        Achievement::create(['category_id' => 4, 'name' => 'journal_scopus', 'value' => 5]);
+        Achievement::create(['category_id' => 4, 'name' => 'journal_wos', 'value' => 4]);
+        Achievement::create(['category_id' => 4, 'name' => 'sinta_account', 'value' => 2788, 'automated' => true]);
         Achievement::create(['category_id' => 4, 'name' => 'total_staff', 'value' => 2793]);
     }
 }
