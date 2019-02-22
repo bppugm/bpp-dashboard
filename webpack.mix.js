fed632df28c4b3e21582ecc57ffd64158af5aa3b
w1lldone <wildan.arrahman@gmail.com>
2019-02-22 13:08:45 +0700

    compile production assets

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,4 +13,6 @@ let mix = require('laravel-mix');
 mix.disableSuccessNotifications();
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+   .sass('resources/assets/sass/app.scss', 'public/css')
+   .sass('resources/assets/sass/toastr.scss', 'public/css')
+   .version();
