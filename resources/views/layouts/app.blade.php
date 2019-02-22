ebd7e99dc01ce9d5c5887b9ce6c52c3949ff8cd7
w1lldone <wildan.arrahman@gmail.com>
2019-02-22 13:08:27 +0700

    import toastr css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset(mix('js/app.js')) }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
@@ -19,7 +19,7 @@
     <link rel="stylesheet" href="{{ asset('font-awesome/css/fontawesome-all.css') }}">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset(mix('css/app.css')) }}" rel="stylesheet">
 
 </head>
 <body>
