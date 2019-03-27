5fc4cecb696febf3f88390ca1f65ab44b2f8fdf0
w1lldone <wildan.arrahman@gmail.com>
2019-03-27 10:08:28 +0700

    Update frontend

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -2,12 +2,9 @@
 
 @section('content')
   <div class="container-fluid">
-    <div class="row pb-5 mb-5">
-      <div class="col-12 py-2 d-flex align-items-center justify-content-center">
-        <img src="{{ url('images/logos/signature-logo-ugm.svg') }}" width="75" class="mr-3">
-        <h1 class="page-title text-primary display-4 font-weight-bold" style="font-size: 3rem;">
-          UGM's 2018 Achievements Minute by Minute
-        </h1>
+    <div class="row pb-5 mb-5 justify-content-center">
+      <div class="col-12 col-md-8 py-2">
+        <img src="{{ url('images/logos/logo-text-ugm.svg') }}" class="img-fluid">
       </div>
     </div>
     <achievement-base 
