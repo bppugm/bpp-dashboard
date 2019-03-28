eb317db592aefe5922328586e30bc29f5c6f45e0
w1lldone <wildan.arrahman@gmail.com>
2019-03-28 10:00:57 +0700

    Exclude indonesian articles in Microsoft Academic

--- app/Clients/PublicationMaClient.php
+++ app/Clients/PublicationMaClient.php
@@ -9,9 +9,30 @@ class PublicationMaClient extends MicrosoftAcademicBase
 {
 	public function fetchData()
 	{
+		$allPublication = $this->getAllPublications();
+		$indonesianPublication = $this->getIndonesianPublication();
+
+		return $allPublication - $indonesianPublication;
+	}
+
+	protected function getAllPublications()
+	{
 		$response = $this->client->post('calchistogram', [
 			'form_params' => [
 				'expr' => "AND(Composite(AA.AfN=='gadjah mada university'),Y=2019)"
+			]
+		]);
+
+		$results = json_decode($response->getBody(), true);
+
+		return $results['num_entities'];
+	}
+
+	protected function getIndonesianPublication()
+	{
+		$response = $this->client->post('calchistogram', [
+			'form_params' => [
+				'expr' => "AND(Composite(AA.AfN=='gadjah mada university'),Y=2019,OR(L='id',L='en@@@id'))"
 			]
 		]);
 
