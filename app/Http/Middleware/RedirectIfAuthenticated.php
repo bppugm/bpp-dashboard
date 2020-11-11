b03446678429cca63c572152dff70514b5c10a93
w1lldone <wildan.arrahman@gmail.com>
2020-11-11 10:38:17 +0700

    Fix redirection

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -18,7 +18,7 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return route('admin.dashboard.index');
+            return redirect()->route('admin.dashboard.index');
         }
 
         return $next($request);
