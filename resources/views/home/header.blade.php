3816b43fcbc3ada74b8ddd3792a7cedba1e2c819
w1lldone <wildan.arrahman@gmail.com>
2021-03-18 08:50:31 +0700

    Fix logo

---
+++ resources/views/home/header.blade.php
@@ -0,0 +1,7 @@
+<div class="py-2 d-flex justify-content-center align-items-center">
+    <img src="{{ url('images/logos/logo-text-ugm.svg?v=3') }}" class="img-fluid" style="height: 7.5rem">
+    <div class="text-primary ml-4" style="font-family: serif;">
+        <h1 class="m-0" style="font-size: 2.5rem; color: #004a74">UNIVERSITAS GADJAH MADA</h1>
+        <h2 style="color: #004a74">2021 ACHIEVEMENTS MINUTES BY MINUTES</h2>
+    </div>
+</div>
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -3,8 +3,8 @@
 @section('content')
   <div class="container-fluid">
     <div class="row pb-5 mb-5 justify-content-center">
-      <div class="col-12 col-md-8 py-2">
-        <img src="{{ url('images/logos/logo-text-ugm.svg?v=2') }}" class="img-fluid">
+      <div class="col-12 col-md-8">
+        @include('home.header')
       </div>
     </div>
     <achievement-base 
